65bdc3f99d8c14c9bc9a546dda89e59764423b1f
Kenneth Mei <meikenneth321@gmail.com>
2022-05-02 23:03:37 -0400

    commit tree a64ccf5a8bcacc75bca92456a096a90b52005622

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const input = document.getElementById('input');
+
 function addingEventListener() {
+    alert('I was clicked!');
+}
+    
+input.addEventListener('click', addingEventListener);
+
+function addingEventListener() {
+    const input = document.getElementById('input');
+
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+    
+    input.addEventListener('click', clickAlert);
 }
